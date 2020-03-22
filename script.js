15c04317a1893bca5aa1aa5b16f8d0c797cbb4e6
Kenan Kadic <ken.kadic@outlook.com>
2020-03-22 18:15:24 -0500

    another commit

--- script.js
+++ script.js
@@ -2,13 +2,30 @@
 let statusOfLaunch = null;
 
 window.addEventListener("load", function() {
+            fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+            response.json().then( function(json) {
+            const div = document.getElementById("missionTarget");
+            // Add HTML that includes the JSON data
+            div.innerHTML = `
+            <h2> Mission Destination </h2>   
+            <ol>
+               <li>Name: ${json[5].name}</li>
+               <li>Diameter: ${json[5].diameter}</li>
+               <li>Star: ${json[5].star}</li>
+               <li>Distance from Earth: ${json[5].distance}</li>
+               <li>Number of Moons: ${json[5].moons}</li>
+            </ol>
+            <img src="${json[5].image}">
+            `;
+            });
+      });
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       let pilotNameInput = document.querySelector("input[name=pilotName]");
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-      let statusOfLaunch = document.querySelector("h2");
+      let statusOfLaunch = document.getElementById("launchStatus");
       let statusOfFuel = document.getElementById("fuelStatus");
       let statusOfPilot = document.getElementById("pilotStatus");
       let pilot = document.getElementById("copilotStatus");
@@ -18,19 +35,19 @@ if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInp
          event.preventDefault();
            }
       else if (!isNaN(pilotNameInput.value)) {
-         alert ("A name is required for Pilot!");
+         alert ("Make sure to enter valid information for each field!");
          event.preventDefault();
       }   
       else if (!isNaN(copilotNameInput.value)) {
-         alert ("A name is required for Copilot!");
+         alert ("Make sure to enter valid information for each field!");
          event.preventDefault();
       }
       else if (isNaN(fuelLevelInput.value)) {
-         alert ("A number is required for FuelLevel!");
+         alert ("Make sure to enter valid information for each field!");
          event.preventDefault();
       }  
       else if (isNaN(cargoMassInput.value)) {
-         alert ("A number is required for cargoMass!");
+         alert ("Make sure to enter valid information for each field!");
          event.preventDefault();
       } 
       else if (Number(fuelLevelInput.value) < 10000) {
